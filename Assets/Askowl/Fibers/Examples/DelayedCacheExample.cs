6aeb1abe60beed048315d74695029568cbfa78df
paulmarrington <paul@marrington.net>
2019-02-03 17:08:00 +1000

    03/02/2019

---
+++ Assets/Askowl/Fibers/Examples/DelayedCacheExample.cs
@@ -0,0 +1,33 @@
+﻿// Copyright 2018 (C) paul@marrington.net http://www.askowl.net/unity-packages
+
+// ReSharper disable MissingXmlDoc
+
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+#if UNITY_EDITOR && AskowlFibers
+namespace Askowl.Examples {
+  public sealed class DelayedCacheExample {
+    private class DelayedCachedData : DelayedCache<DelayedCachedData> {
+      public int Number;
+    }
+
+    [UnityTest] public IEnumerator DelayedCache() {
+      var data = DelayedCachedData.Instance;
+      data.Frames = 10;
+      data.Number = 11;
+      var node       = Cache<DelayedCachedData>.Entries.ReverseLookup(data);
+      var recycleBin = node.Owner.RecycleBin;
+
+      data.Dispose();
+
+      yield return new WaitForSeconds(0.1f);
+      Assert.IsFalse(recycleBin == node.Owner);
+
+      yield return new WaitForSeconds(0.1f);
+      Assert.IsTrue(recycleBin == node.Owner);
+    }
+  }
+}
+#endif
---
+++ Assets/Askowl/Fibers/Scripts/DelayedCache.cs
@@ -0,0 +1,19 @@
+﻿// Copyright 2019 (C) paul@marrington.net http://www.askowl.net/unity-packages
+
+using System;
+using Askowl;
+/// <a href=""></a> //#TBD#//
+public class DelayedCache<T> : IDisposable where T : DelayedCache<T> {
+  /// <a href=""></a> //#TBD#//
+  public static T Instance => Cache<T>.Instance;
+
+  /// <a href=""></a> //#TBD#//
+  public int Frames = 10;
+
+  protected DelayedCache() => disposalFiber = Fiber.Instance.SkipFrames(Frames).Do(_ => Cache<T>.Dispose(this as T));
+
+  private readonly Fiber disposalFiber;
+
+  /// <a href=""></a> //#TBD#//
+  public void Dispose() => disposalFiber.Go();
+}
