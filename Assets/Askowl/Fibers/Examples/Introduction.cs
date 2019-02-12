badd7e7adabcfa41ea707b3f76bb2beb078b81ba
paulmarrington <paul@marrington.net>
2019-02-12 15:55:57 +1000

    12/02/2019

---
+++ Assets/Askowl/Fibers/Examples/Introduction.cs
@@ -0,0 +1,37 @@
+﻿// Copyright 2019 (C) paul@marrington.net http://www.askowl.net/unity-packages
+
+using Askowl;
+using UnityEngine;
+using UnityEngine.UI;
+/// Servicing "Introduction to Fibers" scene
+public class Introduction : MonoBehaviour {
+  [SerializeField] private Toggle testsAvailable = default;
+  void Start() {
+    #if AskowlFibers
+    testsAvailable.isOn = true;
+    #else
+    testsAvailable.isOn = false;
+    #endif
+  }
+
+  /// Checkbox to enable or disable tests
+  public void EnableTests(bool testsEnabled) => DefineSymbols.AddOrRemoveDefines(testsEnabled, "AskowlFibers;Fibers");
+
+  /// When "Home Page" button is pressed
+  public void HomePageButton() => Application.OpenURL("http://www.askowl.net/unity-fibers");
+
+  /// When "Home Page" button is pressed
+  public void DocumentationButton() =>
+    Application.OpenURL("https://paulmarrington.github.io/Unity-Documentation/Fibers/");
+
+  /// When "Home Page" button is pressed
+  public void VideosButton() =>
+    Application.OpenURL("https://www.youtube.com/playlist?list=PLufDQjKXLJJpT2Vkz6LJPyrBkm8HNL4UP");
+
+  /// When "Home Page" button is pressed
+  public void DoxygenButton() => Application.OpenURL(
+    "https://paulmarrington.github.io/Unity-Documentation/Fibers/Doxygen/html/annotated.html");
+
+  /// When "Home Page" button is pressed
+  public void SupportButton() => Application.OpenURL("https://www.patreon.com/paulmarrington");
+}
